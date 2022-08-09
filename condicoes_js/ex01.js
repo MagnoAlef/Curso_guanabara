633136e783490f30e2af5f7f55ad45fc47833d3d
Magno <magno.alefb@gmail.com>
2022-08-08 21:53:20 -0300

    curso guanabara

---
+++ condicoes_js/ex01.js
@@ -0,0 +1,9 @@
+var velocidade = 80
+console.log(`sua velocidade e ${velocidade}`)
+if (velocidade > 60 ){
+    ultrapassou = velocidade - 60
+    console.log(`voçê ultrapassou ${ultrapassou}km/h da velocidade permitida por isso foi multado  `)
+}else{
+    console.log('voçê esta dentro da velocidade permitida')
+}
+console.log('use sempre o cinto de segurança')
