9847d96389377bcd322e8397b362063f928870d6
Magno <magno.alefb@gmail.com>
2022-08-08 22:31:35 -0300

    pequeno gafanhoto

---
+++ condicoes_js/ex02.js
@@ -0,0 +1,11 @@
+var idade = 90
+if (idade < 16){
+    console.log('Nao votar')
+}
+else if (idade >=16 && idade < 18 || idade > 65 ){
+
+        console.log(' Voto opcional')
+    }
+else if (idade > 18){
+    console.log('Voto obrigatorio')
+}
